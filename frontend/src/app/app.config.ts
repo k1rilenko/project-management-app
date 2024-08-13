b375296c36d827a36a0e55760ace57d176cb8143
r.kirilenko <cap.spouk@gmail.com>
2024-08-13 13:37:22 +0300

    add ngrx

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withDebugTracing()),
+    provideClientHydration(),
+    provideStore(),
+  ],
 };
